9b0cd87b760e9715981692ce099af3bf7d5c7284
Mansoor Choudhury <mansoorchoudhury@Mansoors-MacBook-Pro.local>
2023-03-30 01:42:10 +0100

    Updated Partners tests

--- BatchReceving/Partners/src/test/java/partners/configurations/LogConfigTest.java
+++ BatchReceving/Partners/src/test/java/partners/configurations/LogConfigTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@EnableAutoConfiguration(exclude = {KafkaAutoConfiguration.class})
 public class LogConfigTest {
 
     @Autowired
