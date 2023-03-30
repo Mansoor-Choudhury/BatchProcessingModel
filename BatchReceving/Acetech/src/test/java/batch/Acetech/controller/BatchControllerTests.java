443e0f87b4ecce86f6f0e62ab22728b4f29c2869
Mansoor Choudhury <mansoorchoudhury@Mansoors-MacBook-Pro.local>
2023-03-30 01:34:30 +0100

    Optimising imports

--- BatchReceving/Acetech/src/test/java/batch/Acetech/controller/BatchControllerTests.java
+++ BatchReceving/Acetech/src/test/java/batch/Acetech/controller/BatchControllerTests.java
@@ -3,7 +3,6 @@ package batch.Acetech.controller;
 import batch.Acetech.model.Batch;
 import batch.Acetech.model.BatchResponse;
 import batch.Acetech.service.BatchService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
--- BatchReceving/Partners/src/test/java/partners/configurations/LogConfigTest.java
+++ BatchReceving/Partners/src/test/java/partners/configurations/LogConfigTest.java
@@ -1,17 +1,11 @@
 package partners.configurations;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import partners.service.BatchPartnerConsumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
